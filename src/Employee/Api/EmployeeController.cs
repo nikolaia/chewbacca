6522d54871febeb6db540780e8e1324b3c1e04e4
TrymVei <tve@variant.no>
2022-10-26 08:38:57 +0200

    Feat: add posibility to get data from cv partner with employeeController api

--- src/Employee/Api/EmployeeController.cs
+++ src/Employee/Api/EmployeeController.cs
@@ -1,7 +1,11 @@
+using CvPartner.DTOs;
+
 using Employee.Repositories;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
+using Refit;
 
 namespace Employee.Api;
 
@@ -11,6 +15,12 @@ public class EmployeeController : ControllerBase
 {
     private readonly EmployeeContext _db;
     private readonly ILogger<EmployeeController> _logger;
+     public interface IEmployeeApi
+    {
+        [Headers("Authorization: Token token=6151d4be9aa0fc8f2e51367b57a9c78b")]
+        [Get("/users?")]
+        Task<IEnumerable<CVPartnerUserDTO>> GetAllEmployee();
+    }
 
     public EmployeeController(EmployeeContext db, ILogger<EmployeeController> logger)
     {
@@ -19,11 +29,15 @@ public class EmployeeController : ControllerBase
     }
 
     [HttpGet]
-    public List<Models.Employee> Get()
+    public async Task<IEnumerable<CVPartnerUserDTO>> Get()
     {
-        _logger.LogInformation("Getting employees from database");
-        
-        var employees = _db.Employees.ToList();
+        // _logger.LogInformation("Getting employees from database");
+        var cvPartnerApi = RestService.For<IEmployeeApi>("https://variant.cvpartner.com/api/v1");
+        var employees = await cvPartnerApi.GetAllEmployee();
+
         return employees;
+
+        // var employees = _db.Employees.ToList();
+        // return employees;
     }
 }
