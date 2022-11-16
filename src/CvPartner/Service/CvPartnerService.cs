e5889837bb11c17d0a83453b45c9fa1473165f2c
TrymVei <tve@variant.no>
2022-11-16 14:31:09 +0100

    fix: Removed unused referances

--- src/CvPartner/Service/CvPartnerService.cs
+++ src/CvPartner/Service/CvPartnerService.cs
@@ -1,33 +1,15 @@
 using CvPartner.Models;
 using CvPartner.Repositories;
 
-using Employees.Models;
-using Employees.Service;
-
 namespace CvPartner.Service;
 
 public class CvPartnerService
 {
     private readonly CvPartnerRepository _cvPartnerRepository;
-    private readonly EmployeesService _employeeService;
 
-    public CvPartnerService(CvPartnerRepository _cvPartnerRepository, EmployeesService _employeeService)
+    public CvPartnerService(CvPartnerRepository _cvPartnerRepository)
     {
         this._cvPartnerRepository = _cvPartnerRepository;
-        this._employeeService = _employeeService;
-    }
-
-    private static EmployeeEntity ConvertToEmployeeEntity(CVPartnerUserDTO dto)
-    {
-        EmployeeEntity employee = new()
-        {
-            Name = dto.name,
-            Email = dto.email,
-            Telephone = dto.telephone,
-            OfficeName = dto.office_name,
-            ImageUrl = dto.image.url,
-        };
-        return employee;
     }
 
     /**
