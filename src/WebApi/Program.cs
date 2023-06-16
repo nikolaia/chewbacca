8452e3e3e5afe48b189e41f798e67a316d6f7906
Dora Oline Eriksrud <doraoline@gmail.com>
2023-06-16 14:22:20 +0200

    Add CORS-policy

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -51,6 +51,14 @@ builder.Configuration
     .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    });
+});
+
 // Bind configuration "TestApp:Settings" section to the Settings object
 var appSettingsSection = builder.Configuration
     .GetSection("AppSettings");
@@ -125,6 +133,8 @@ builder.Services.AddScheduler(ctx =>
 
 var app = builder.Build();
 
+app.UseCors("MyCorsPolicy");
+
 /*
  * Migrate the database.
  * Ideally the app shouldn't have access to alter the database schema, but we do it for simplicity's sake,
@@ -133,6 +143,12 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<EmployeeContext>();
+
+    if (app.Environment.IsDevelopment())
+    {
+        db.Database.EnsureCreated();
+    }
+
     var isInMemoryDatabase = db.Database.ProviderName?.StartsWith("Microsoft.EntityFrameworkCore.InMemory") ?? false;
     if (!isInMemoryDatabase)
     {
