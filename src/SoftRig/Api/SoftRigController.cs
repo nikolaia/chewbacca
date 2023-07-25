7a8b6ee67ecd153b492705e3dab18919914c17de
Dora Oline Eriksrud <doraoline@gmail.com>
2023-07-25 11:30:44 +0200

    Remove code used for development

--- src/SoftRig/Api/SoftRigController.cs
+++ src/SoftRig/Api/SoftRigController.cs
@@ -15,50 +15,12 @@ public class SoftRigController : ControllerBase
         this._softRigService = softRigService;
     }
 
-    [HttpGet("/softrig/companyKey")]
-    [OutputCache(Duration = 60)]
-    public async Task<string> GetCompanyKey()
-    {
-        var token = await _softRigService.RequestTokenAsync();
-        return await _softRigService.GetCompanyKey(token.AccessToken!, "Variant");
-    }
-
-    // TODO: just during development
-    [HttpGet("/softrig/employee/update")]
-    [OutputCache(Duration = 60)]
-    public async Task<SoftRigEmployee> TestUpdateEmployee([FromQuery] string email)
-    {
-        var token = await _softRigService.RequestTokenAsync();
-        var employee = await _softRigService.GetSoftRigEmployee(token.AccessToken!, email);
-        var testInformation = new SoftRigEmployeeDto
-        {
-            Phone = "11223344",
-            AccountNumber = "12341234123",
-            Address = "En annen adresse",
-            ZipCode = "1234",
-            City = "Sarpsborg"
-        };
-
-        var updateSuccess = await _softRigService.UpdateEmployee(email, testInformation);
-
-        return await _softRigService.GetSoftRigEmployee(token.AccessToken!, email);
-    }
-
-    // TODO: just during development
-    [HttpGet("/softrig/employee")]
-    [OutputCache(Duration = 60)]
-    public async Task<SoftRigEmployee> GetEmployee([FromQuery] string email)
-    {
-        var token = await _softRigService.RequestTokenAsync();
-        return await _softRigService.GetSoftRigEmployee(token.AccessToken!, email);
-    }
-
-    // TODO: just during development
-    [HttpGet("/softrig/employees")]
-    [OutputCache(Duration = 60)]
-    public async Task<List<SoftRigEmployee>> GetAllEmployees()
-    {
-        var token = await _softRigService.RequestTokenAsync();
-        return await _softRigService.GetSoftRigEmployees(token.AccessToken!);
-    }
+    // Use during development
+    // [HttpGet("/softrig/companyKey")]
+    // [OutputCache(Duration = 60)]
+    // public async Task<string> GetCompanyKey()
+    // {
+    //     var token = await _softRigService.RequestTokenAsync();
+    //     return await _softRigService.GetCompanyKey(token.AccessToken!, "Variant");
+    // }
 }
