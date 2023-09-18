e9f4e51fb27057ee6ea754f5d345a51d8eba78fd
nikolaia <nikolai@stasj.com>
2023-09-18 10:59:03 +0200

    fix: Move ArchitectureTests and update README

--- ArchitectureTests/Usings.cs
+++
@@ -1,6 +0,0 @@
-global using Xunit;
-global using ArchUnitNET.Domain;
-global using ArchUnitNET.Fluent;
-global using ArchUnitNET.Loader;
-global using ArchUnitNET.xUnit;
-global using static ArchUnitNET.Fluent.ArchRuleDefinition;
